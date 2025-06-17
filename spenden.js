1206f203faf5614244362210c5bc8fb7600ae22e
networksbeni <networks.afi@sz.ch>
2025-06-17 19:23:00 +0200

    Update spenden.js

--- spenden.js
+++ spenden.js
@@ -11,5 +11,6 @@ const SPENDEN = [
     { firma: "Test Firma 1", spende: 50.95},
     { firma: "Test Firma 3", spende: 51},
     { firma: "Test Firma 4", spende: 40},
+    { firma: "Roller 2", spende: 350.99},
     { firma: "Highroller", spende: 400}
 ];
